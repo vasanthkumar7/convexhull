5848a81c5135207cafeeefcf92041301b0512359
vasanth kumar(maan petter) <48177734+vasanthkumar7@users.noreply.github.com>
2020-05-14 21:21:19 +0530

    Update convexhull.py

--- convexhull.py
+++ convexhull.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-x=[1,3,3,1,2,3,5,1,9,10,5]
-y=[1,1,3,3,7,2,9,1,4,2,9]
+x=[1,3,1,7,9,5,5,4,3,2]
+y=[2,4,3,8,3,6,5,4,8,6]
 p=min(y)
 pp=y.index(p)
 
@@ -20,43 +20,50 @@ stacky.append(y[e+1])
 stackx.append(x[e+2])
 stacky.append(y[e+2])
 try:
-    angle=((stacky[e+1]-stacky[e])*(stackx[e+2]-stackx[e+1]))-((stacky[e+2]-stacky[e+1])*(stackx[e+1]-stackx[e]))
+    anglep=((stacky[e+1]-stacky[e])/(stackx[e+1]-stackx[e]))-((stacky[e+2]-stacky[e+1])/(stackx[e+2]-stackx[e+1]))
 except ZeroDivisionError:
-    angle=0
+    anglep=0
 
-if(angle>=0):
-    stackx.remove(x[e+1])
-    stacky.remove(y[e+1])
-if(angle<0):
+if(anglep>=0):
+    del stackx[e+1]
+    del stacky[e+1]
+if(anglep<0):
     e=e+1
-
-print(angle)
-print(stackx,stacky,sep=' ')
-print(len(stackx))
+print("e=",e)
+print("angle= ",anglep)
+print(" ")
 for i in range(len(stackx),len(x)):
         stackx.append(x[i])
         stacky.append(y[i])
+       
         w=0
         wq=0
+        print(stackx,stacky,sep=' ')
+       
         try:
-            angle=((stacky[e+1]-stacky[e])*(stackx[e+2]-stackx[e+1]))-((stacky[e+2]-stacky[e+1])*(stackx[e+1]-stackx[e]))
-            
+            angle=((stacky[e+1]-stacky[e])/(stackx[e+1]-stackx[e]))-((stacky[e+2]-stacky[e+1])/(stackx[e+2]-stackx[e+1]))
         except ZeroDivisionError:
             angle=0
-        print(stackx,stacky,sep=' ')
-        print(angle)
-        angle=w-wq
+        
+        print(stackx[e+1])
+        print(stacky[e+1])
         if(angle>=0):
-            stackx.remove(stackx[e+1])
-            stacky.remove(stacky[e+1])
+            del stackx[e+1]
+            del stacky[e+1]
         if(angle<0):
             e=e+1
+        print("angle=",angle)
+        print(stackx,stacky,sep=' ')
+        
+        print("e=",e)
+        print(" ")
        
 
-
-        
 stackx.append(stackx[0])
 stacky.append(stacky[0])
+        
+
+print(stackx,stacky,sep=' ')
 plt.title("convex hull") 
 plt.plot(stackx,stacky)
 plt.plot(x,y,'o')
