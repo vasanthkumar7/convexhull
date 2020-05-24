bc2eca977cf547ab60156d94b3ec7da1ba25aef9
vasanth kumar(maan petter) <48177734+vasanthkumar7@users.noreply.github.com>
2020-05-24 20:16:02 +0530

    Update convexhull.py

--- convexhull.py
+++ convexhull.py
@@ -10,7 +10,7 @@ xx=[]
 yy=[]
 for i in range(len(x)):
     
-    if(x[i]!=y[i] or y[i]>=max(y)):
+    if(x[i]!=y[i] or y[i]>=max(y) or y[i]==min(y)):
         xx.append(x[i])
         yy.append(y[i])
 tempx=x
